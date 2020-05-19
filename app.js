5d7b3acf178cab3d705e794a9d321c2f5f5d5781
Erik Gross <erik.d.gross@gmail.com>
2020-05-18 18:42:30 -0700

    Update to finished project

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 function get_todos() {
-    // data access
-    
-    localStorage.setItem('todo', JSON.stringify('Cheese'));
-    var foo = localStorage.getItem('todo');
-
-    return JSON.parse(foo);
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
 }
 
 function add() {
-    // take value of <input> element and insert onto localStorage string
+    var task = document.getElementById('task').value;
+
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    document.getElementById("task").value = "";
+    show();
+
+    return false;
 }
 
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+}
 
 function show() {
-    var dbCall = get_todos();
+    var todos = get_todos();
 
-    todos = "<ul>" + "<li>" + "Eggs" + "</li>" + "<li>" + dbCall + "</li>" + "</ul>";
-    document.getElementById("todos").innerHTML = todos;  
+    var html = '<ul>';
+    for (var i = 0; i < todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+
+    var buttons = document.getElementsByClassName('remove');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
 }
 
+document.getElementById('add').addEventListener('click', add);
 show();
